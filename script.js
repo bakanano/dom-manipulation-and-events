1ad3718aafe9ff7578d47fcf2bbd2540ecd904ae
bakanano <narutoz0rs@live.com>
2021-12-11 16:51:48 +1300

    Finished step 2

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ p.classList.add("red");
 p.textContent = "Hey I'm red!";
 p.style.color = "red";
 document.body.appendChild(p);
+
+const h3 = document.createElement("h3");
+h3.classList.add("blue");
+h3.textContent = "I'm a blue h3!";
+h3.style.color = "blue";
+document.body.appendChild(h3);
