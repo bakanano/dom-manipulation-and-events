4487d0be2d633638bc5fb0cbe5ac9649a0c7e290
bakanano <narutoz0rs@live.com>
2021-12-15 15:32:19 +1300

    Finished js-drum-kit exercise

--- wesbos-javascript30/01-javascript-drum-kit/app.js
+++ wesbos-javascript30/01-javascript-drum-kit/app.js
@@ -1,4 +1,25 @@
+window.addEventListener("keydown", playSound);
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) {
+        return;
+    } else {
+        key.classList.add("playing")
+        audio.currentTime = 0;
+        audio.play();
+    }
+}
+
 const keys = Array.from(document.querySelectorAll(".key"));
-keys.addEventListener("keydown", (e) => {
-    console.log(e);
-})
+keys.forEach((key) => key.addEventListener("transitionend", removeEffect));
+
+function removeEffect(e) {
+    if (e.propertyName !== "transform") {
+        return;
+    } else {
+        e.target.classList.remove("playing");
+    }
+}
+
